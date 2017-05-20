1685739baf84651cc5728a2fca1ccf8004d73b2e
novellito <christian.trinidad.501@my.csun.edu>
2017-05-20 14:32:37 -0700

    first commit

---
+++ js/twitFunctions.js
@@ -0,0 +1,92 @@
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+
+//create a new tweet
+module.exports.makeTweet = (input) => {
+
+    var tweet = {
+        status: input
+    }
+
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response) {
+        if (err) {
+            console.log('there was an error in making the tweet!');
+        } else {
+
+            console.log('tweeted successfully!');
+        }
+    }
+}
+
+module.exports.followUser = (user) => {
+    T.post('friendships/create', {
+        screen_name: user
+    }, followed);
+
+    function followed(err, data, response) {
+        if (err) {
+            console.log('there was an error in following!');
+        } else {
+            console.log('You followed: ' + user);
+        }
+    }
+}
+
+module.exports.retweet = (something) => {
+    var params = {
+        q: something, //it has to recently exist to work
+        result_type: 'recent',
+        lang: 'en'
+    }
+
+    T.get('search/tweets', params, function (err, data) {
+
+        if (!err) {
+
+            var retweetId = data.statuses[0].id_str; //retrieves the first status essentially 
+            T.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function (err, response) {
+                if (response) {
+                    console.log('retweeted');
+                }
+                if (err) {
+                    console.log('something went wrong with retweeting!');
+                }
+            });
+        }
+    });
+}
+
+module.exports.like = (something) => {
+    var params = {
+        q: something,
+        result_type: 'recent',
+        lang: 'en'
+
+    }
+
+    T.get('search/tweets', params, function (err, data) {
+
+        var tweet = data.statuses;
+        var randTweet = random(tweet);
+
+        T.post('favorites/create', {
+            id: randTweet.id_str
+        }, function (err, response) {
+            if (err) {
+                console.log('error has occured when liking!');
+            } else {
+                console.log('favoriting went well!');
+            }
+        });
+    });
+}
+
+function random (arr) {
+  var index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+};
---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var exphbs = require('express-handlebars');
+var twitFun = require('./js/twitFunctions');
+
+///set up templating engine
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+//middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+
+app.get('/', function (req, res) {
+  res.render('home')
+});
+
+app.post('/', function (req, res) {
+  if(typeof(req.body.theTweet) !== 'undefined'){
+    twitFun.makeTweet(req.body.theTweet);
+    res.send(req.body);
+
+  }
+  if(typeof(req.body.theUser) !== 'undefined'){
+    twitFun.followUser(req.body.theUser);
+    res.send(req.body);
+
+  }
+  if(typeof(req.body.theRetweet) !== 'undefined'){
+    twitFun.retweet(req.body.theRetweet);
+    res.send(req.body);
+
+  }
+  if(typeof(req.body.theLike) !== 'undefined'){
+    twitFun.like(req.body.theLike);
+    res.send(req.body);
+
+  }
+
+  
+
+});
+
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+});
