130b8da99c05214076b520b1c6ce75c0d5b1946e
novellito <christian.trinidad.501@my.csun.edu>
2017-05-21 17:30:07 -0700

    fixed old keys

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 //credentials for twitter api
 module.exports = {
-  consumer_key:         'BcnExQBoQJ1xDVuNMto2Q0B8t',
-  consumer_secret:      'f9V72HW23egFyCpamcTunTf503Nl5AHdmjMz9CoIQoo8DjtfIx',
-  access_token:         '821946765414789121-f0rcKJuN3540FJr5jsa4SbfiLI6sW6n',
-  access_token_secret:  'sT4HAevB0vmNfxMru6XRI0UOYq1lGfIQjSNjCxXqRDpG8',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
 }
