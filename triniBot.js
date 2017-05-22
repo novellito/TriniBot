504d3cffb36fb247ca4975fd01e657778241976e
novellito <christian.trinidad.501@my.csun.edu>
2017-05-21 17:23:09 -0700

    fixed files

--- triniBot.js
+++ triniBot.js
@@ -11,20 +11,16 @@ stream.on('tweet', function (tweet) {
 
     var replyTo = tweet.user.screen_name;
     var analyzeText = sentiment(tweet.text);
-
+    var name = tweet.user.name;
     if (analyzeText.score < 0) {
-        console.log(tweet.text);
-        makeTweet('MuCh sAdnEsS vErY sAd DoGe NoT wOw @' + replyTo);
+        makeTweet('MuCh sAdnEsS vErY sAd DoGe NoT wOw @' + replyTo + ' no love ' + name);
     } else if (analyzeText.score > 0) {
-        console.log(tweet.text);
-        makeTweet('MuCh nIcE HaPpY dOge sUcH jOy wOw @' + replyTo);
+        makeTweet('MuCh nIcE HaPpY dOge sUcH jOy wOw @' + replyTo + ' much love ' + name);
     } else {
-        console.log(tweet.text);
-        makeTweet('very neutral doge :) @' + replyTo);
+        makeTweet('very neutral doge :) @' + replyTo + ' equal love ' + name);
     }
-
     i++; 
-    if (i == 14) {
+    if (i == 5) {
         i = 0;
         stream.stop();
         setTimeout(() => {
@@ -34,27 +30,28 @@ stream.on('tweet', function (tweet) {
     }
 });
 
-var streamReply = T.stream('user')
+var streamReply = T.stream('user');
 streamReply.on('tweet', function (tweet) {
+
 
     var mention = tweet.in_reply_to_screen_name;
     var replyTo = tweet.user.screen_name;
+    var username = tweet.user.name;
     if (mention === 'trini_bot') {
-        var rep = '@' + replyTo + ' vErY tWeEt MuCh SoCiAl DoGe'
+        var rep = '@' + replyTo + ' vErY tWeEt MuCh SoCiAl DoGe ' + username + ' is awesome!';
         makeTweet(rep);
     }
 
 });
 
 var followStream = T.stream('user');
-followStream.on('follow', followed);
-
-function followed(e) {
-    console.log('someone followed you!');
+followStream.on('follow', function(e){
+ console.log(e.source.name + ' followed you!');
     var name = e.source.name;
     var screenName = e.source.screen_name;
-    makeTweet('@' + screenName + ' MuCh foLlOwErS VeRy ThAnKs');
-}
+    makeTweet('@' + screenName + ' MuCh foLlOwErS VeRy ThAnKs ' + name + ' is the greatest!');
+});
+
 
 function makeTweet(input) {
 
@@ -67,6 +64,7 @@ function makeTweet(input) {
     function tweeted(err, data, response) {
         if (err) {
             console.log('there was an error in making the tweet!');
+            console.log(err);
         } else {
 
             console.log('tweeted successfully!');
